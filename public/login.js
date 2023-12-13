1af5b06688a0dd19368abdeaa38e94ab65f11b65
Aria Christensen <ariacchristensen@gmail.com>
2023-12-12 21:02:39 -0700

    added third party endpoint - random quote on login page

--- public/login.js
+++ public/login.js
@@ -3,4 +3,22 @@ function login() {
     localStorage.setItem("userName", nameEl.value);
     window.location.href = "allRecipes.html";
   }
+
+/**
+  https://github.com/lukePeavey/quotable
+ */
+function displayQuote(data) {
+  document.getElementById("quote").textContent = data.content;
+  document.getElementById("author").textContent = data.author;
+}
+
+function callService(url, displayCallback) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      displayCallback(data);
+    });
+}
+
+callService("https://api.quotable.io/random", displayQuote);
   
